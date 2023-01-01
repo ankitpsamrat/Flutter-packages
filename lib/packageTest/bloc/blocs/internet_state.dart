0c69668f28d0eef29e43338aea7e3dddb25e9b41
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-01-01 18:28:02 +0530

    first commit

--- lib/packageTest/bloc/blocs/internet_state.dart
+++
@@ -1,8 +0,0 @@
-abstract class InternetState {}
-
-class InternetInitialState extends InternetState {}
-
-class InternetLostState extends InternetState {}
-
-class InternetGainedState extends InternetState {}
-
