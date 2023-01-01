25f648c032423a8968a88a35460a55d5c9ed9a4f
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-01-01 18:29:01 +0530

    first commit

---
+++ lib/bloc/blocs/internet_bloc.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test/bloc/blocs/internet_event.dart';
+import 'package:test/bloc/blocs/internet_state.dart';
+
+class InternetBloc extends Bloc<InternetEvent, InternetState> {
+  //  created variable
+
+  final _connectivity = Connectivity();
+  StreamSubscription? connectivitySubscription;
+
+  //  bloc method
+
+  InternetBloc() : super(InternetInitialState()) {
+    on<InternetLostEvent>((event, emit) => emit(InternetLostState()));
+    on<InternetGainedEvent>((event, emit) => emit(InternetGainedState()));
+
+    connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen((result) {
+      if (result == ConnectivityResult.mobile ||
+          result == ConnectivityResult.wifi) {
+        add(InternetGainedEvent());
+      } else {
+        add(InternetLostEvent());
+      }
+    });
+  }
+
+  @override
+  Future<void> close() {
+    connectivitySubscription?.cancel();
+    return super.close();
+  }
+}
