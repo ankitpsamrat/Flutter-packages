41909134e63a15e393f7ed5d1c9b7d0a393563a6
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-04-26 23:38:12 +0530

    code added

---
+++ lib/carouselSlider/home_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class MySlider extends StatefulWidget {
+  @override
+  _MySliderState createState() => _MySliderState();
+}
+
+class _MySliderState extends State<MySlider> {
+  int _currentIndex = 0;
+
+  final List<Widget> _containers = [
+    Container(
+      color: Colors.red,
+      child: Center(
+        child: Text(
+          'Container 1',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ),
+    Container(
+      color: Colors.green,
+      child: Center(
+        child: Text(
+          'Container 2',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ),
+    Container(
+      color: Colors.blue,
+      child: Center(
+        child: Text(
+          'Container 3',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CarouselSlider(
+          items: _containers,
+          options: CarouselOptions(
+            height: 200,
+            viewportFraction: 1.0,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: _containers.asMap().entries.map((entry) {
+            final index = entry.key;
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+              child: Container(
+                width: 12.0,
+                height: 12.0,
+                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _currentIndex == index
+                      ? Colors.blue
+                      : Colors.grey.withOpacity(0.5),
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              _currentIndex = (_currentIndex + 1) % _containers.length;
+            });
+          },
+          child: Text('Next'),
+        ),
+      ],
+    );
+  }
+}
