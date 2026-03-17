9d2a9860aee3583fdc46b6557850a51e8ddd8508
ankitpsamrat <ankit.p.samrat@gmail.com>
2026-03-17 23:10:07 +0530

    removed bloc

--- lib/main.dart
+++ lib/main.dart
@@ -20,24 +20,6 @@ class MyApp extends StatelessWidget {
       home: SlidingUpScreen(),
     );
 
-    //  for bloc method
-    // return BlocProvider(
-    //   create: (context) => InternetBloc(),
-    //   child: MaterialApp(
-    //     debugShowCheckedModeBanner: false,
-    //     home: BlocHome(),
-    //   ),
-    // );
-
-    //  for cubit mehtod
-    // return BlocProvider(
-    //   create: (context) => InternetCubit(),
-    //   child: MaterialApp(
-    //     debugShowCheckedModeBanner: false,
-    //     home: CubitHome(),
-    //   ),
-    // );
-
     //  for go router method
     // return MaterialApp.router(
     //   debugShowCheckedModeBanner: false,
